56c1f510dbcc09389cd6358a575a525afafb751c
Gabriel <Gabriel-Polveiro12@gmail.com>
2022-01-26 04:18:17 -0300

    codigo vitao

--- src/App.js
+++ src/App.js
@@ -18,19 +18,18 @@ function App() {
   }
   
   useEffect(async () => {
-     const response = await fetch("api.openweathermap.org/data/2.5/weather?lat=`${location.latitude}`&lon=`${location.longitude}`&appid=0a2418234df7417984e84ffa6f0fcddf");
+     const response = await fetch("api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=0a2418234df7417984e84ffa6f0fcddf");
      const data = await response.json();
 
-     console.log(response)
-
      setWeather(data);
+     console.log(data);
   }, [])
 
   return (
     <div className="App">
      <>
-      Latitude: {location.latitude}
-      Longitude: {location.longitude}
+      <h1>Latitude: {location.latitude}</h1>
+      <h1>Longitude: {location.longitude}</h1>
      </>
     </div>
   );
